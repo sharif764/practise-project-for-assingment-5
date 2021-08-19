f3aebc22cb801c54f2283db541af08ff1b33712d
sharif764 <ss1884088@gmail.com>
2021-08-20 02:36:29 +0600

    jj

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-const ol = document.getElementById('ol');
+/* const ol = document.getElementById('ol');
 ol.addEventListener('click',function(event){
     const newElement = document.createElement('li');
     newElement.innerText = 'no-11';
     ol.appendChild(newElement);
-});
-const inputField = document.getElementById('input-field');
+}); */
+/* const inputField = document.getElementById('input-field');
 const button = document.getElementById('btn');
 button.addEventListener('click',function(){
     const inputField = document.getElementById('input-field');
     if(inputField.value==5){
-         button.setAttribute('disabled','disabled');
-    }
+        button.setAttribute('disabled','disabled');
+   }
     else{
     const previousValue = parseInt(inputField.value);
     const newInputValue = previousValue+1;
@@ -19,4 +19,11 @@ button.addEventListener('click',function(){
     createElement.innerText = 'new';
     ol.appendChild(createElement);
     }
-})
+}); */
+const number = [9,44,56,35,56,3,56];
+number.sort(function(a,b){
+  if(a>b)return 1;
+  if(a<b)return -1;
+  return 0;
+});
+console.log(number);
---
+++ lederBoard.js
@@ -0,0 +1,13 @@
+const homeNav = document.getElementById('home-nav');
+homeNav.style.backgroundColor = 'skyblue';
+const navDivId = document.getElementById('nav-div-id');
+const ideaDiv = document.getElementById('idea');
+/* ideaDiv.style.display = 'flex';
+ideaDiv.style.justifyItems = 'spaceBetween'; */
+navDivId.style.display = 'flex';
+navDivId.style.justifyItems = 'spaceBetween';
+const buttonArea = document.getElementById('button-area');
+buttonArea.style.display = 'flex';
+buttonArea.style.justifyContent = 'spaceBetween';
+const body = document.body;
+body.style.backgroundColor = 'rgba(229, 93, 131, 1)';
