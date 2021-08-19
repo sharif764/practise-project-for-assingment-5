f2dd314a5fda378bb13b560d83ecbbf33558db0a
sharif764 <ss1884088@gmail.com>
2021-08-19 23:14:58 +0600

    jj

--- app.js
+++ app.js
@@ -7,5 +7,16 @@ ol.addEventListener('click',function(event){
 const inputField = document.getElementById('input-field');
 const button = document.getElementById('btn');
 button.addEventListener('click',function(){
- 
+    const inputField = document.getElementById('input-field');
+    if(inputField.value==5){
+         button.setAttribute('disabled','disabled');
+    }
+    else{
+    const previousValue = parseInt(inputField.value);
+    const newInputValue = previousValue+1;
+    inputField.value = newInputValue;
+    const createElement = document.createElement('li');
+    createElement.innerText = 'new';
+    ol.appendChild(createElement);
+    }
 })
